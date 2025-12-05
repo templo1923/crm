81394ff5e6cbb8b23089990bb0db3f4b5d998022
Maiko <maiko@catalogo.alwaysdata.net>
2025-12-05 10:24:21 -0500

    ok

--- content/index.js
+++ content/index.js
@@ -51,4 +51,4 @@ function __vitePreload(baseModule, deps, importerUrl) {
         }
         return baseModule().catch(handlePreloadError);
     });
-}__vitePreload(()=> import(chrome.runtime.getURL("content/assets/js/v_7_4_2_13_59773d94-6a68-4ed1-8b86-dcf9791b0ab720.js")),[]);__vitePreload(()=> import(chrome.runtime.getURL("content/assets/js/v_7_4_2_13_59773d94-6a68-4ed1-8b86-dcf9791b0ab721.js")).then(r=>r.v),[]);
+}__vitePreload(()=> import(chrome.runtime.getURL("content/assets/js/v_7_4_2_18_ea7d0884-20ea-4e04-b6f3-cef449ac182f20.js")),[]);__vitePreload(()=> import(chrome.runtime.getURL("content/assets/js/v_7_4_2_18_ea7d0884-20ea-4e04-b6f3-cef449ac182f21.js")).then(r=>r.v),[]);
--- funnil/assets/js/02033519-a506-4138-a99e-39f66c643695.ZWIs3vTI.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./5735e24c-11d1-46e6-966f-07dd43dc3c68.0Z0pQ5H2.js";var a=r();export{a as r};
